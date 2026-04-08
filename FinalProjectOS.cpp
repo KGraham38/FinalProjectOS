06a61fb7a4a259b715af0c8ce8b052efd2769b3f
Kody Graham <KGraham.cs@gmail.com>
2026-04-08 00:06:37 -0500

    Added some simple step outlines and all of the standard imports i think i need

--- FinalProjectOS.cpp
+++ FinalProjectOS.cpp
@@ -1,9 +1,34 @@
-// FinalProjectOS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Kody Graham
+//04/05/2026
+//OS Final Project - CSCI 4300
+//I have decided to do FIFO to really be able to compare how much better optimal is compared to the worst case scenerio 
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Page replacement FIFO
+    
+    //Step 1: Read/parse text from file; Argument1 (first letter either F, O, or L) = replacement algorithm, Argument2 = number of memory frames (second char in file must be an int), and Argument3= the reference string(comma seperated)
+    string fileName;
+
+
+    //Step 2: Fifo Algorithm 
+
+    //Step 3: Output
+    //Step 3.a: Calculate num of page faults 
+    
+    //Step 3.b: Display like the figure in the final project instruction.
+    //Basically show all of the input string values but only show the pages when a page fault occurs.
+   
+    //Other implementation output notes: 
+    //Seperator is a number of dashes corresponding to the length of the input string, looks like about 6x dashes per num of input string values
+    //Just one space to the right of each string value
+
+
 }
