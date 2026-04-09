fc96726eab81cad6d3a2057429d1f7c2bd8e1243
Kody Graham <KGraham.cs@gmail.com>
2026-04-08 21:10:42 -0500

    Done with repeat loop

--- FinalProjectOS.cpp
+++ FinalProjectOS.cpp
@@ -12,134 +12,145 @@ using namespace std;
 
 int main()
 {
-    //Page replacement FIFO
-    
     //Step 1: Read/parse text from file; Argument1 (first letter either F, O, or L) = replacement algorithm, Argument2 = number of memory frames (second char in file must be an int), and Argument3= the reference string(comma seperated)
     string fileName;
     string input;
     ifstream inputFile;
+    string repeat = "y";
 
-    cout << "Enter the input file name: ";
-    cin >> fileName;
-
-    //Open the file
-    inputFile.open(fileName);
-
-    //Make sure file is open
-    if (!inputFile)
+    while (repeat == "y" || repeat == "Y")
     {
-        cout << "ERROR: Could not open the file " << fileName << endl;
-        return 1;
-    }
 
-    //Read the first line of the file and save in input
-    getline(inputFile, input);
+        cout << "Enter the input file name: ";
+        cin >> fileName;
 
-    //Close the file
-    inputFile.close();
+        //Open the file
+        inputFile.open(fileName);
 
-    //Store the arguments
-    string argumentsArray[1000];
-    int argCount = 0;
-    string currentArg = "";
+        //Make sure file is open
+        if (!inputFile)
+        {
+            cout << "ERROR: Could not open the file " << fileName << endl;
+            return 1;
+        }
 
-    //Split the input based on the commas
-    for (int i = 0; i < input.length(); i++)
-    {
-        if (input[i] == ',')
+        //Read the first line of the file and save in input
+        getline(inputFile, input);
+
+        //Close the file
+        inputFile.close();
+
+        //Store the arguments
+        string argumentsArray[1000];
+        int argCount = 0;
+        string currentArg = "";
+
+        //Split the input based on the commas
+        for (int i = 0; i < input.length(); i++)
+        {
+            if (input[i] == ',')
+            {
+                argumentsArray[argCount] = currentArg;
+                argCount++;
+                currentArg = "";
+            }
+            else
+            {
+                currentArg += input[i];
+            }
+        }
+
+        //Save last arg
+        if (currentArg != "")
         {
             argumentsArray[argCount] = currentArg;
             argCount++;
-            currentArg = "";
+        }
+
+        //Make sure all 3 required args are present
+        if (argCount < 3)
+        {
+            cout << "Error: To few arguments.\n";
+            cout << "Required File Format : Algorithm letter, Number of Frames, and the Reference String\n";
+            return 1;
+        }
+
+        //First arg = algorithm letter
+        char algorithm = argumentsArray[0][0];
+
+        //Second arg = num of memory frames
+        int numFrames = stoi(argumentsArray[1]);
+
+        //Everything else = reference string
+        int referenceString[1000];
+        int numOfReferences = 0;
+
+        //Starting at 2 because first 2 are the other arguments
+        for (int i = 2; i < argCount; i++)
+        {
+            referenceString[numOfReferences] = stoi(argumentsArray[i]);
+            numOfReferences++;
+        }
+
+        //Basic Validation
+        if (algorithm == 'F' || algorithm == 'f')
+        {
+            cout << "This file is marked to use the FIFO Algorithm\n";
+        }
+        else if (algorithm == 'O' || algorithm == 'o')
+        {
+            cout << "This file is marked to use the OPT Algorithm\n";
         }
         else
         {
-            currentArg += input[i];
+            cout << "Error: Argument for Algorithm Name is not valid.\n";
+            cout << "This program can only be used for the FIFO or Optimal algorithms!\n";
+
+            return 1;
         }
-    }
-        
-     //Save last arg
-     if (currentArg != "")
-     {
-         argumentsArray[argCount] = currentArg;
-         argCount++;
-     }
 
-     //Make sure all 3 required args are present
-     if (argCount<3)
-     {
-         cout << "Error: To few arguments.\n";
-         cout << "Required File Format : Algorithm letter, Number of Frames, and the Reference String\n";
-         return 1;
-     }
+        if (numFrames <= 0)
+        {
+            cout << "Error: Number of frames must be greater than 0.\n";
+            return 1;
+        }
 
-     //First arg = algorithm letter
-     char algorithm = argumentsArray[0][0];
-     
-     //Second arg = num of memory frames
-     int numFrames = stoi(argumentsArray[1]);
+        if (numOfReferences == 0)
+        {
+            cout << "Error: Reference string not found.\n";
+            return 1;
+        }
 
-     //Everything else = reference string
-     int referenceString[1000];
-     int numOfReferences = 0;
+        //Test section to confirm input
+        cout << endl;
+        cout << "Algorithm: " << algorithm << endl;
+        cout << "Number of Frames: " << numFrames << endl;
+        cout << "Reference String: ";
 
-     //Starting at 2 because first 2 are the other arguments
-     for (int i = 2; i < argCount; i++)
-     {
-         referenceString[numOfReferences] = stoi(argumentsArray[i]);
-         numOfReferences++;
-     }
+        for (int i = 0; i < numOfReferences; i++)
+        {
+            cout << referenceString[i] << " ";
+        }
+        cout << endl;
 
-     //Basic Validation
-     if (algorithm == 'F' || algorithm == 'f')
-     {
-        cout << "This file is marked to use the FIFO Algorithm\n";
-     }
-     else if (algorithm == 'O' || algorithm == 'o')
-     {
-         cout << "This file is marked to use the OPT Algorithm\n";
-     }
-     else
-     {
-         cout << "Error: Argument for Algorithm Name is not valid.\n";
-         cout << "This program can only be used for the FIFO or Optimal algorithms!\n";
+        //Split info section from real output section       
+        for (int i = 0; i < numOfReferences; i++)
+        {
+            cout << "____";
+        }
 
-         return 1;
-     }
+        cout << endl;
 
-     if (numFrames <= 0)
-     {
-         cout << "Error: Number of frames must be greater than 0.\n";
-         return 1;
-     }
+        cout << "Repeat, Y or N? :";
+        cin >> repeat;
 
-     if (numOfReferences == 0)
-     {
-         cout << "Error: Reference string not found.\n";
-         return 1;
-     }
 
-     //Test section to confirm input
-     cout << endl;
-     cout << "Algorithm: " << algorithm << endl;
-     cout << "Number of Frames: " << numFrames << endl;
-     cout << "Reference String: ";
-
-     for (int i = 0; i < numOfReferences; i++)
-     {
-         cout << referenceString[i] << " ";
-     }
-     cout << endl;
-
-     //Split info section from real output section
-     int dashLength = numOfReferences * 3;
-       
-    for (int i = 0; i < dashLength; i++)
-    {
-            cout << "___";
     }
 
-    cout << endl;
+    cout << "Thank You for using our program!" << endl << "Goodbye!" << endl;
+
+
+    
     
 
     //Step 2: Fifo Algorithm 
